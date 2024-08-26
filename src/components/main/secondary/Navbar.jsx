0d0284a82e5450e3d29c50079a7beeef1fee8d47
Lian Ivan Sinchez <maquiavelic1030@gmail.com>
2024-08-26 13:56:42 -0300

    test

--- src/components/main/secondary/Navbar.jsx
+++ src/components/main/secondary/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(){
         <header>
             <ul id="navbar">
                 <li><a href="../index.html">Inicio</a></li>
-                <li><a href="../pages/buscar.html">Buscar</a></li>
+                <li><a href="../pages/buscar.html">Search</a></li>
                 <li><a href="../pages/categoria.html">Explorar por categoria</a></li>
                 <li><a href="../pages/likes.html">Tus me gusta</a></li>
                 <li><a href="../pages/perfil.html">Tu perfil</a></li>
