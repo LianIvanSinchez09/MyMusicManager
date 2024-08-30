7882211de0a343783f63a26c31b17a4a55182385
Lian Ivan Sinchez <maquiavelic1030@gmail.com>
2024-08-30 19:20:19 -0300

    pre tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
 import { albums } from "../../classes/songs";
 
-
-
 export default function Home(){
-    console.log(albums);
     return(
-        
+                
         <section id="main-content">
             <h1>hola mundo desde home</h1>
         </section>
